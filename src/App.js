cbe873e8319b71ca31b6b94e36e51b2a10a50e0a
Aryan Majeed <aryanmajid97@gmail.com>
2021-11-27 23:45:01 +0300

    build: add tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+        Button
+      </button>
+    </div>
+  );
 }
 
 export default App;
